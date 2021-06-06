f52a5b725816a91bf697ed6fa201748e7601a787
Cwaihenya <cwaihenya.gmail.com>
2021-06-06 16:14:59 +0900

    Changes to pirvacy and root setting

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -63,13 +63,13 @@ class PostsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_post
-      @post = Post.find(params[:id])
-    end
+
+  def set_post
+  @post = Post.find(params[:id])
+  end
 
     # Only allow a list of trusted parameters through.
-    def post_params
-      params.require(:post).permit(:tweet)
-    end
+  def post_params
+  params.require(:post).permit(:tweet)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
       post:confirm
     end
     end
+    root to: "posts#new"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
